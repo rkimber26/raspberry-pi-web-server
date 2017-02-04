9e095dd8e209b5e649568b653f133c80667b33b7
rkimber26 <kimberrob@btinternet.com>
2017-02-04 08:02:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,6 @@ def bob():
 def robert():
     return 'robert is the best!'
 
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
